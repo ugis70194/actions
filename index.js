ffce62625e0a2c491eb57f71bdae5e1d3bd5d745
ugis70194 <ugis70194@yahoo.co.jp>
2023-07-14 17:16:21 +0900

    a

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const { Toolkit } = require('actions-toolkit');
+const fs = require('fs');
 
-try {
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const targetDir = "./contents"
+
+Toolkit.run(async tools => {
+  try {
+    fs.readdir('.', (err, files) => {
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
+  } catch (error) {
+
+  }
+})
