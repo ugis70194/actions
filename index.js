808acaaadd8cbab4d00b98f5114e8feb0b193b18
ugis70194 <ugis70194@yahoo.co.jp>
2023-07-14 17:47:02 +0900

    a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Toolkit } = require('actions-toolkit');
 const fs = require('fs');
 
-const targetDir = process.env.GITHUB_WORKSPACE+"/contents";
+const targetDir = process.env.GITHUB_WORKSPACE;
 
 Toolkit.run(async tools => {
   console.log(targetDir);
