a58b880dd82531e75b330cde9bbdf515577d4de5
ugis70194 <ugis70194@yahoo.co.jp>
2023-07-14 17:20:44 +0900

    a

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const fs = require('fs');
 const targetDir = "./contents"
 
 Toolkit.run(async tools => {
+  console.log("toolkit run");
   try {
     fs.readdir('.', (err, files) => {
         files.forEach(file => {
             console.log(file);
         });
     });
-  } catch (error) {
-
+  } catch (e) {
+    tools.log.fatal(e);
+    tools.exit.failure('Failed to increment the value.');
   }
 })
